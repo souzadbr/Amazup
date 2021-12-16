cd4f5655b1a0894a0b75ad82f3b7ff1875e5f226
Debora Souza <deboral.p@hotmail.com>
2021-12-16 10:36:44 -0300

    Ajustar erro na ControllerAdvisor

--- src/main/java/br/com/zup/Amazup/configs/ControllerAdvisor.java
+++ src/main/java/br/com/zup/Amazup/configs/ControllerAdvisor.java
@@ -1,6 +1,8 @@
 package br.com.zup.Amazup.configs;
 
+import br.com.zup.Amazup.configs.exception.LivroJaCadastradoException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,22 +11,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@RestControllerAdvice
 public class ControllerAdvisor {
-    @RestControllerAdvice
-    public class ControllerAdivisor {
-
-        @ExceptionHandler(MethodArgumentNotValidException.class)
-        @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-        public List<ErroDeValidacao> manipularErrosDeValidacao(MethodArgumentNotValidException exception) {
-            List<ErroDeValidacao> erros = new ArrayList<>();
-            for (FieldError fieldError : exception.getFieldErrors()) {
-                ErroDeValidacao erroDeValidacao = new ErroDeValidacao(
-                        fieldError.getDefaultMessage(), fieldError.getField());
-                erros.add(erroDeValidacao);
-            }
-            return erros;
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public List<ErroDeValidacao> manipularErrosDeValidacao(MethodArgumentNotValidException exception) {
+        List<ErroDeValidacao> erros = new ArrayList<>();
+        for (FieldError fieldError : exception.getFieldErrors()) {
+            ErroDeValidacao erroDeValidacao = new ErroDeValidacao(
+                    fieldError.getDefaultMessage(), fieldError.getField());
+            erros.add(erroDeValidacao);
         }
+        return erros;
+    }
+
+    @ExceptionHandler(LivroJaCadastradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MensagemDeErro livroJaCadastrado(LivroJaCadastradoException exception) {
+        return new MensagemDeErro("Livro já cadastrado");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public MensagemDeErro naoLegivelException() {
+        return new MensagemDeErro("Informação do JSON ilegível");
+
     }
 }
-
---
+++ src/main/java/br/com/zup/Amazup/configs/MensagemDeErro.java
@@ -0,0 +1,2 @@
+package br.com.zup.Amazup.configs;public class MensagemDeErro {
+}
